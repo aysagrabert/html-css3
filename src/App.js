60042826c369d0766107b47e1a413599eb704689
Aysa Grabert <aysagrabert@Aysas-MacBook-Pro.local>
2019-10-23 14:05:00 -0600

    finished

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+ constructor() {
+   super()
+   this.dropdown = React.createRef()
+ }
+//  this.state = {
 
+//  }
+
+
+ render() {
+   return (
+    <div>
+      <div className='img'>
+        <nav>
+          
+          <div className='name'>Start Bootstrap</div>
+
+          <div id='button-container'>
+            <button className='button'>SERVICES</button>
+            <button className='button'>PORTFOLIO</button>
+            <button className='button'>ABOUT</button>
+            <button className='button'>TEAM</button>
+            <button className='button'>CONTACT</button>
+          </div>
+        </nav>
+
+        <div className='word-container'>
+          <div className='welcome'>Welcome To Our Studio!</div>
+          <div className='meet'>IT'S NICE TO MEET YOU</div>
+          <div className='tell'>TELL ME MORE</div>
+      </div>
+
+      </div>
+
+
+
+    </div>
+   )
+ }
+}
 export default App;
