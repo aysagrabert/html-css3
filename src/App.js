efa64e8781eab2d590709095ae0fb12f1910126f
Aysa Grabert <aysagrabert@Aysas-MacBook-Pro.local>
2019-10-29 12:22:51 -0600

    finished it

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './reset.css';
 import './App.css';
 
 class App extends React.Component {
@@ -6,40 +7,67 @@ class App extends React.Component {
    super()
    this.dropdown = React.createRef()
  }
-//  this.state = {
+ animate = () => {
+   this.setState({
+    
+   })
+ }
 
-//  }
+ toggleShow = () => {
+   console.log(this.dropdown.current)
+   let {current} = this.dropdown
 
+   if(current.classList.contains('show-animation')){
+    current.classList.add('hide-animation')
+    current.classList.remove('show-animation')
+   } else{
+    current.classList.add('show-animation')
+    current.classList.remove('hide-animation')
+   }
+ }
 
  render() {
    return (
-    <div>
-      <div className='img'>
+      <div>
         <nav>
-          
-          <div className='name'>Start Bootstrap</div>
 
-          <div id='button-container'>
-            <button className='button'>SERVICES</button>
-            <button className='button'>PORTFOLIO</button>
-            <button className='button'>ABOUT</button>
-            <button className='button'>TEAM</button>
-            <button className='button'>CONTACT</button>
-          </div>
+
+        <div className='name'>Start Bootstrap</div>
+
+          <i id='hamburger-icon'  className='fas fa-bars fa-2x' onClick={this.toggleShow}/>
+
+         <div id='button-container'>
+            <div className='button'>SERVICES</div>
+            <div className='button'>PORTFOLIO</div>
+            <div className='button'>ABOUT</div>
+            <div className='button'>TEAM</div>
+            <div className='button'>CONTACT</div>
+         </div>
+
         </nav>
 
+        <div className='dropdown' ref={this.dropdown}>
+            <div className='dropdown-button'>Home</div>
+            <div className='dropdown-button'>About</div>
+            <div className='dropdown-button'>Services</div>
+            <div className='dropdown-button'>Contact</div>
+        </div>
+
+        
         <div className='word-container'>
           <div className='welcome'>Welcome To Our Studio!</div>
+
           <div className='meet'>IT'S NICE TO MEET YOU</div>
+
           <div className='tell'>TELL ME MORE</div>
+          
       </div>
+      
 
-      </div>
-
-
-
-    </div>
+        </div>
+    
    )
  }
 }
 export default App;
+
